ca09799fa896439595630728aced8eca6b03747f
Martin Barreto <mtn.barreto@gmail.com>
2021-12-30 13:46:54 -0300

    component to check if user if logged in

---
+++ src/components/privateRoute.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+// import { navigate } from "gatsby"
+//import { isLoggedIn } from "../services/auth"
+
+const PrivateRoute = ({ component: Component, location, ...rest }) => {
+  // if (!isLoggedIn() && location.pathname !== `/accounts/sign-in`) {
+  //   navigate("/accounts/sign-in")
+  //   return null
+  // }
+  return <Component {...rest} />
+}
+export default PrivateRoute
