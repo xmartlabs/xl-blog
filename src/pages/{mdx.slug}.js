7fdd915240e01db8b4a35a6919bef6e40e7adbfb
Martin Barreto <mtn.barreto@gmail.com>
2022-04-12 14:14:46 -0300

    link authors and category pages from post page.

--- src/pages/{mdx.slug}.js
+++ src/pages/{mdx.slug}.js
@@ -1,16 +1,17 @@
 import * as React from 'react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import AuthorsYAMLData from "../../content/authors.yaml"
 import CategoriesYAMLDATA from "../../content/categories.yaml"
+const _ = require("lodash")
 
 const BlogPost = ({ data }) => {
   const author = AuthorsYAMLData.find(author => { return author.author === data.mdx.frontmatter.author })
   const category = CategoriesYAMLDATA.find(category => { return category.category === data.mdx.frontmatter.category })
   return (
     <>
-        <div>{ category.title }</div>
-        <div>{ author.display_name }</div>
+        <div><Link to={`/categories/${ _.kebabCase(category.category) }`}>{ category.title }</Link></div>
+        <div><Link to={`/authors/${ _.kebabCase(author.author) }`}>{ author.display_name }</Link></div>
         <div>{ author.image }</div>
         <div>{ author.profile_url }</div>
         <p>{data.mdx.frontmatter.date}</p>
