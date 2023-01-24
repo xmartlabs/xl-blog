e2826d3ec1b54e685f6cbc2af47efa396d535c42
Melissa <melissa.martinez@xmartlabs.com>
2023-01-24 13:57:35 -0300

    Finished the social disappear

--- src/components/social-blog/social-blog.jsx
+++ src/components/social-blog/social-blog.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { Twitter, Facebook, Linkedin } from "../icons";
+import { classnames } from "../../helpers/utils";
 
 import * as styles from "./social-blog.module.scss";
 
-const SocialBlog = () => (
-  <div className={styles.container}>
+const SocialBlog = ({ className }) => (
+  <div className={classnames(styles.container, className)}>
     <a href="https://twitter.com/xmartlabs"><Twitter className={styles.socialIcon} /></a>
     <a href="https://www.facebook.com/xmartlabs/" className={styles.facebookSpace}><Facebook className={styles.socialIcon} /></a>
     <a href="https://www.linkedin.com/company/xmartlabs/"><Linkedin className={styles.socialIcon} /></a>
--- src/pages/{mdx.frontmatter__permalink}.js
+++ src/pages/{mdx.frontmatter__permalink}.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { graphql, Link } from 'gatsby';
@@ -22,33 +22,53 @@ const BlogPost = ({ data }) => {
   const authorBlog = AuthorSerializer.deSerialize(author);
   const categoryBlog = CategorySerializer.deSerialize(category);
   const { setState } = useContext(AppContext);
+  const [ disappearSocial, setDisappearSocial ] = useState(false);
 
   useEffect(() => {
     setState(BannerType.blog);
+    
+    window.addEventListener('scroll', handleScroll, {
+      passive: true
+    });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
+  const handleScroll = () => {
+    const bottom = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight;
+    if (bottom) {
+      setDisappearSocial(true);
+    } else {
+      if (!disappearSocial) {
+        setDisappearSocial(false);
+      }
+    };
+  };
+
   return (
-    <>
-    <SocialBlog />
-      <div className={styles.bannerContainer}>
-        <Category data={categoryBlog.displayName}/>
-        <h1 className={classnames(styles.titleContainer, "text__heading__one__black")}>
-          { data.mdx.frontmatter.title }
-        </h1>
-        <div className={styles.authorContainer}>
-          <div className={styles.authorInformation}>
-            <img src={`images/${authorBlog.image}`} alt="" className={styles.authorImage} />
-            <Link className={classnames(styles.authorName, "text__paragraph__bold__black")} to={`/authors/${ _.kebabCase(authorBlog.author) }`}>{ authorBlog.displayName }</Link>
+    <div onScroll={handleScroll}>
+      <SocialBlog className={disappearSocial && styles.socialDisappear} />
+        <div className={styles.bannerContainer}>
+          <Category data={categoryBlog.displayName}/>
+          <h1 className={classnames(styles.titleContainer, "text__heading__one__black")}>
+            { data.mdx.frontmatter.title }
+          </h1>
+          <div className={styles.authorContainer}>
+            <div className={styles.authorInformation}>
+              <img src={`images/${authorBlog.image}`} alt="" className={styles.authorImage} />
+              <Link className={classnames(styles.authorName, "text__paragraph__bold__black")} to={`/authors/${ _.kebabCase(authorBlog.author) }`}>{ authorBlog.displayName }</Link>
+            </div>
+            <label className={classnames(styles.postDate, "text__label__bold__grayTwo")} >{data.mdx.frontmatter.date}</label>
           </div>
-          <label className={classnames(styles.postDate, "text__label__bold__grayTwo")} >{data.mdx.frontmatter.date}</label>
         </div>
-      </div>
       <div className={styles.bodyPostContainer}>
         <MDXRenderer>
           {data.mdx.body}
         </MDXRenderer>
       </div>
-    </>
+    </div>
   );
 };
 
