3b51f3c1c63606bbd3f9db2fbd494524964c6fc4
Melissa <melissa.martinez@xmartlabs.com>
2023-02-01 14:18:41 -0300

    Back to the start

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -59,7 +59,7 @@ export const StyledFooterText = styled.label`
 
 function Layout({ children }) {  
   const [contextState, setContextState] = useState(initialState);
-  const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
   const [ handleMenu, setHandleMenu ] = useState(false);
 
   const handleWindowSizeChange = () => {
@@ -67,8 +67,6 @@ function Layout({ children }) {
   }
 
   useEffect(() => {
-    const size = window.innerWidth;
-    setWidth(size);
     window.addEventListener('resize', handleWindowSizeChange);
     return () => {
       window.removeEventListener('resize', handleWindowSizeChange);
