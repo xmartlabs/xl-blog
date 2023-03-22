9c1eef6499e06d9f6eeed90dca0d550b401ee503
Melissa <melissa.martinez@xmartlabs.com>
2023-03-22 16:32:12 -0300

    Resolving <strong> label in titles and blogs with only h3 headings

--- src/helpers/find-titles.js
+++ src/helpers/find-titles.js
@@ -3,14 +3,37 @@ import React from "react";
 const findTitles = (titles) => {
   const h1List = [];
   const h2List = [];
+  const h3List = [];
   titles.map((title) => {
     if (title.nodeName === "H1") {
-      title.setAttribute("id", title.innerHTML);
-      h1List.push(title);
+      if (title.innerHTML.includes('<strong>')) {
+        title.innerHTML = title.innerHTML.slice(8);
+        title.setAttribute("id", title.innerHTML);
+        h1List.push(title);
+      } else {
+        title.setAttribute("id", title.innerHTML);
+        h1List.push(title);
+      }
     }
     if (title.nodeName === "H2") {
-      title.setAttribute("id", title.innerHTML);
-      h2List.push(title);
+      if (title.innerHTML.includes('<strong>')) {
+        title.innerHTML = title.innerHTML.slice(8);
+        title.setAttribute("id", title.innerHTML);
+        h2List.push(title);
+      } else {
+        title.setAttribute("id", title.innerHTML);
+        h2List.push(title);
+      }
+    }
+    if (title.nodeName === "H3") {
+      if (title.innerHTML.includes('<strong>')) {
+        title.innerHTML = title.innerHTML.slice(8);
+        title.setAttribute("id", title.innerHTML);
+        h3List.push(title);
+      } else {
+        title.setAttribute("id", title.innerHTML);
+        h3List.push(title);
+      }
     }
   });
   if (h1List.length !== 0) {
@@ -18,6 +41,10 @@ const findTitles = (titles) => {
   } else {
     if (h2List.length !== 0) {
       return h2List;
+    } else {
+      if (h3List.length !== 0) {
+        return h3List;
+      }
     }
   }
   return [];
