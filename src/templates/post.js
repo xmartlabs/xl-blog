47fd6ecb6176278743af31d070a76f956a89bba3
Melissa <melissa.martinez@xmartlabs.com>
2023-04-04 17:01:13 -0300

    Changing de function to ES6

--- src/templates/post.js
+++ src/templates/post.js
@@ -132,13 +132,12 @@ const BlogPost = ({ data }) => {
     return null;
   }; 
 
-  const disqusBody = () => {
-    var disqus_shortname = 'xmartlabs';
-    return (function() {
-      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
+  var disqus_shortname = 'xmartlabs';
+
+  const disqusCharge = () => {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   }
 
   return (
@@ -182,7 +181,7 @@ const BlogPost = ({ data }) => {
       <div className={styles.disqusSection}>
         <h3 className={styles.disqusTitle}>Comments:</h3>
         <div id="disqus_thread"></div>
-          {disqusBody()}
+          {disqusCharge()}
         <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
       </div>
     </div>
