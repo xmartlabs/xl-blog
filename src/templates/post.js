490af5751e2aa9a39ce47514225e1817c8f96957
Melissa <melissa.martinez@xmartlabs.com>
2023-04-04 15:57:56 -0300

    Disqus title

--- src/templates/post.js
+++ src/templates/post.js
@@ -95,15 +95,12 @@ const BlogPost = ({ data }) => {
    }; 
 
    const disqusBody = () => {
-   /* * * CONFIGURATION VARIABLES * * */
-   var disqus_shortname = 'xmartlabs';
-
-   /* * * DON'T EDIT BELOW THIS LINE * * */
-   return (function() {
-       var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-       dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-       (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-   })();
+    var disqus_shortname = 'xmartlabs';
+    return (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
    }
 
   return (
