359ba99e17281166ea2f3361cfc60a06398dcf9b
Melissa <melissa.martinez@xmartlabs.com>
2023-06-27 16:50:54 -0300

    Deleting unused properties in category serializer

--- src/serializer/category-serializer.js
+++ src/serializer/category-serializer.js
@@ -2,10 +2,7 @@ class CategorySerializer {
   static deSerialize(data) {
     return {
       category: data.category,
-      postTitle: data.title,
       displayName: data.display_name,
-      url: data.url,
-      color: data.color,
     };
   }
 }
