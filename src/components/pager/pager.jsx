4ea53c7f665455e138c4ac045346d65af8b538c7
Melissa <melissa.martinez@xmartlabs.com>
2023-07-19 13:45:31 -0300

    Change pager logic

--- src/components/pager/pager.jsx
+++ src/components/pager/pager.jsx
@@ -6,16 +6,18 @@ import PropTypes from "prop-types";
 import * as styles from "./pager.module.scss";
 
 const buildPageLocation = ({ numPages, page, category }) => {
-  if (page === numPages) {
-    return '';
+
+  if (page - 1 < 1) {
+    return category ? `/categories/${category}` : `/`;
   }
 
-  if (!category && page === 1) {
-    return '/'
+  if (page + 1 === numPages) {
+    return '';
   }
 
   return category ? `/categories/${category}/page/${page}` : `/page/${page}`;
 };
+
 
 const Pager = ({ pageContext }) => {
   const {numPages,  currentPage, category} = pageContext;
@@ -32,7 +34,7 @@ const Pager = ({ pageContext }) => {
 
   return(
     <div className={styles.pagerContainer}>
-      <Link className={currentPage === 1 ? styles.disabledPagerLink : styles.pagerLink} to={prevPage} rel="prev">
+      <Link className={currentPage <= 1 ? styles.disabledPagerLink : styles.pagerLink} to={prevPage} rel="prev">
         ← Prev
       </Link>
         <Link to={actualPage} className={styles.linkNumber}>{currentPage}</Link>
