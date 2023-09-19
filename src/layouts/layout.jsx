f1d43d2cd1b9bffd4b1e2f3ed6bea727b245a086
Melissa <melissa.martinez@xmartlabs.com>
2023-09-19 12:16:41 -0300

    Add typeform

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -133,7 +133,7 @@ function Layout({ children }) {
                   {!isMobile && <NavMenu />}
                 </div>
                 {!isMobile && 
-                <StyledGetStartedButton className={styles.getStarted} id="header-getintouch" href={process.env.GATSBY_CONTACT_FORM}>
+                <StyledGetStartedButton className={styles.getStarted} id="header-getintouch" href="https://form.typeform.com/to/c7G2RUWm">
                   <StyledGetStartedTextButton>GET STARTED</StyledGetStartedTextButton>
                 </StyledGetStartedButton>
                 }
