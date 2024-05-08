dfb483bacb608b39d209aab76f28453cbd0b5a4c
Gabriel <gabriel.machin@xmartlabs.com>
2024-05-08 10:57:41 -0300

    Add image metadata to blog page

--- src/components/seo/seo.jsx
+++ src/components/seo/seo.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useSiteMetadata } from "../../hooks/use-site-metadata"
 import { withPrefix } from "gatsby-link"
 
-export const Seo = ({ title, description, pathname, children }) => {
+export const Seo = ({ title, description, pathname, children, seoImageUrl }) => {
   const { title: defaultTitle, description: defaultDescription, siteUrl } = useSiteMetadata()
 
   const seo = {
@@ -16,7 +16,7 @@ export const Seo = ({ title, description, pathname, children }) => {
       <meta property="og:title" content={seo.title} />
       <meta property="og:url" content={seo.url} />
       <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={`${siteUrl}/images/og-thumbnail.png`} />
+      <meta property="og:image" content={seoImageUrl || `${siteUrl}/images/og-thumbnail.png`} />
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <script src={withPrefix('identity.js')} type="text/javascript"></script>
